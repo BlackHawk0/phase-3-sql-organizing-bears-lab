a7203593e6f42331482bb4502e3e568d8ad41c81
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-26 18:15:17 +0300

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 22, 'M', 'blue','sanguine', true ),
+('Rowdy', 25, 'M', 'blue','sanguine', true ),
+('Tabitha', 27, 'F', 'pink','melancholic', true ),
+('Sergeant Brown', 21, 'M', 'blue','sanguine', true ),
+('Melissa', 20, 'F', 'purple','melancholic', true ),
+('Grinch', 26, 'M', 'brown','sanguine', true ),
+('Wendy', 29, 'F', 'violet',' choleric', true ),
+(NULL, NULL, 'F', NULL, NULL, Null);
